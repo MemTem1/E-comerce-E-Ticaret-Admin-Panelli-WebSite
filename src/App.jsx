eca9dfc630ec17f270363214d8199ee23329dff2
memol <memoli.2606@icloud.com>
2025-12-26 23:04:17 +0300

    Add Blogs section with BlogItem component
    Introduced a new Blogs section to the layout, including BlogItem and associated CSS files. Updated App.jsx to render the Blogs section. Also improved Campaigns.css with new styles for the campaigns wrapper.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Sliders from './component/Layout/Slider/Sliders.jsx'
 import Categories from './component/Layout/Categories/Categories.jsx'
 import Product from './component/Layout/Products/Product.jsx'
 import Campaigns from './component/Layout/Campaigns/Campaigns.jsx'
+import Blogs from './component/Layout/Blogs/Blogs.jsx'
 
 function App() {
 
@@ -16,6 +17,8 @@ function App() {
       <Categories />
       <Product />
       <Campaigns />
+      <Product />
+      <Blogs />
       <Policy />
       <Footer />
 
