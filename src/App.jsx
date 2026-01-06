c607314fb9599544239ffc013e4ef0718b68b272
memol <memoli.2606@icloud.com>
2026-01-06 05:39:39 +0300

    shopi page
    shop page içeriği dizayn edildi ve gerekli componentler eklemesi yapıldı

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import HomePage from './pages/HomePage.jsx'
-
+import ShopPage from './pages/ShopPage.jsx'
 import './App.css'
 function App() {
 
   return (
     <>
-      <HomePage />
+      {/* <HomePage /> */}
+      <ShopPage />
     </>
   )
 }
--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import Header from '../component/Layout/header/Header'
+import Categories from '../component/Layout/Categories/Categories.jsx'
+import Product from '../component/Layout/Products/Product.jsx'
+import CampaignSingle from '../component/Layout/CampaignSingle/CampaignSingle'
+import Policy from '../component/Layout/policy/Policy.jsx'
+import Footer from '../component/Layout/footer/Footer.jsx'
 function ShopPage() {
     return (
-        <div>ShopPage</div>
+        <>
+            <Header />
+            <Categories />
+            <Product />
+            <CampaignSingle />
+            <Product />
+            <Policy />
+            <Footer />
+        </>
     )
 }
 
